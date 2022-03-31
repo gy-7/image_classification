f5c94f7b33e4790cb7a77e7ad6762e07549a9ea2
gy77 <64619863+gy-7@users.noreply.github.com>
2022-03-31 17:54:54 +0800

    update resnet

---
+++ ResNet/model.py
@@ -0,0 +1,201 @@
+# Pytorch官方实现的resnet： https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py
+
+import torch
+import torch.nn as nn
+
+
+# resnet18 and resnet34 block
+class block_18_34(nn.Module):
+    expansion=1
+    # 为了兼容 block_50_101_152 中的expansion，方便后边动态调用，定义一个expansion，
+
+    # 18层，34层的残差结构中，也会有虚线残差结构，也就是进行下采样，调整尺寸以及通道数。
+    # 用stride来控制是否下采样，stride=1：不用下采样，对应的就是实线残差结构；
+    #                       stride=2：需要下采样，对应的就是虚线残差结构。同时也需要传入downsample。
+
+    def __init__(self,in_channel,out_channel,stride=1,downsample=None):
+        super(block_18_34, self).__init__()
+
+        self.conv1=nn.Conv2d(in_channels=in_channel,out_channels=out_channel,
+                        kernel_size=3,stride=stride,padding=1,bias=False)
+        self.bn1=nn.BatchNorm2d(out_channel)
+        self.relu=nn.ReLU()
+
+        self.conv2=nn.Conv2d(in_channels=out_channel,out_channels=out_channel,
+                             kernel_size=3,stride=1,padding=1,bias=False)
+        self.bn2=nn.BatchNorm2d(out_channel)
+        self.downsample=downsample
+
+    def forward(self,x):
+        identity=x
+        if self.downsample is not None:
+            identity=self.downsample(x)
+
+        x=self.conv1(x)
+        x=self.bn1(x)
+        x=self.relu(x)
+
+        x=self.conv2(x)
+        x=self.bn2(x)
+
+        x+=identity
+        x=self.relu(x)
+        return x
+
+# resnet50, resnet101 and resnet34 residual
+class block_50_101_152(nn.Module):
+    # 因为50层，101层，152层的残差结构中，block中卷积的通道数是不一样的。
+    # 都是，最后一个1*1的卷积的通道数，是前两个卷积通道数的四倍。
+    # 所以需要用一个expansion参数，来指定最后一个1*1卷积的通道数。
+    expansion=4
+
+    # 用stride来控制是否下采样，stride=1：不用下采样，对应的就是实线残差结构；
+    #                       stride=2：需要下采样，对应的就是虚线残差结构。同时也需要传入downsample。
+    def __init__(self,in_channel,out_channel,stride=1,
+                 downsample=None):
+        super(block_50_101_152, self).__init__()
+
+        self.conv1=nn.Conv2d(in_channels=in_channel,out_channels=out_channel,
+                             kernel_size=1,stride=1,bias=False)
+        self.bn1=nn.BatchNorm2d(out_channel)
+
+        self.conv2=nn.Conv2d(in_channels=out_channel,out_channels=out_channel,
+                             kernel_size=3,stride=stride,bias=False,padding=1)
+        self.bn2=nn.BatchNorm2d(out_channel)
+
+        self.conv3=nn.Conv2d(in_channels=out_channel,out_channels=out_channel*self.expansion,
+                             kernel_size=1,stride=1,bias=False)
+        self.bn3=nn.BatchNorm2d(out_channel*self.expansion)
+
+        self.relu=nn.ReLU(inplace=True)
+        self.downsample=downsample
+
+    def forward(self,x):
+        identity=x
+        if self.downsample is not None:
+            identity=self.downsample(x)
+
+        x=self.conv1(x)
+        x=self.bn1(x)
+        x=self.relu(x)
+
+        x=self.conv2(x)
+        x=self.bn2(x)
+        x=self.relu(x)
+
+        x=self.con3(x)
+        x=self.bn3(x)
+
+        x+=identity
+        x=self.relu(x)
+        return x
+
+class ResNet(nn.Module):
+    def __init__(self,block,blocks_num,num_classes=1000,include_top=True):
+        super(ResNet, self).__init__()
+        self.include_top=include_top
+        self.in_channel=64
+
+        # conv1
+        self.conv1=nn.Conv2d(in_channels=3,out_channels=self.in_channel,
+                        kernel_size=7,stride=2,padding=3,bias=False)
+        self.bn1=nn.BatchNorm2d(self.in_channel)
+        self.relu=nn.ReLU(inplace=True)
+
+        # conv2_x
+        self.maxpool=nn.MaxPool2d(kernel_size=3,stride=2,padding=1)
+        self.layer1 = self._make_layer(block, 64, blocks_num[0])
+
+        # conv3_x
+        self.layer2 = self._make_layer(block, 128, blocks_num[1], stride=2)
+
+        # conv4_x
+        self.layer3 = self._make_layer(block, 256, blocks_num[2], stride=2)
+
+        # conv5_x
+        self.layer4 = self._make_layer(block, 512, blocks_num[3], stride=2)
+
+        if self.include_top:
+            self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # output size = (1, 1)
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
+            # 加载预训练权重的时候，num_classes要用1000，加载完后，我们再单独修改这个fc层中的num_clasees
+            # 因为官方给的预训练权重是，按照1000分类给的。
+
+        for m in self.modules():    # 初始化
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+
+
+
+
+    def _make_layer(self, block, block_first_channel, block_num, stride=1):
+        downsample = None
+
+        # 如果步距不为1，就说明进行了下采样，我们就需要定义一个downsample，来让走shortcut的特征图尺寸与走卷积层的尺寸相同。
+        if stride != 1 or self.in_channel != block_first_channel * block.expansion:
+
+            # downsample就是shortcut，stride=1，就是实线shortcut，stride=2，就是虚线shortcut。
+            # 在18，34层的resnet中，一个block中的通道数是没有发生变化的，所以shortcut中的通道数也是没有发生变化的。
+            # 在50，101，152层的resnet中，一个block中的通道数是发生变化的，所以shortcut中的通道数也要发生变化，变为原来的四倍。
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channel, block_first_channel * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(block_first_channel * block.expansion))
+
+        layers = []     # 每个stage中先把第一个，可能会使用下采样的block加进来。
+        layers.append(block(self.in_channel,
+                            block_first_channel,
+                            stride=stride,
+                            downsample=downsample))
+        self.in_channel = block_first_channel * block.expansion
+        # 在50，101，152层的resnet中，经历过一个下采样的block后，通道数就会都变为输入时的四倍。
+
+        for _ in range(1, block_num):
+            layers.append(block(block_first_channel * block.expansion, block_first_channel))
+            # 经历过第一个下采样的block后，通道数变为了原来的四倍，例如50层的conv2_x：64 -> 256
+            # 后边两个block，就需要先用block中的第一个1*1的卷积层，进行降维，再将通道数：256 -> 64
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        if self.include_top:
+            x = self.avgpool(x)
+            x = torch.flatten(x, 1)
+            x = self.fc(x)
+
+        return x
+
+def resnet18(num_classes=1000, include_top=True):
+    # https://download.pytorch.org/models/resnet18-f37072fd.pth
+    return ResNet(block_18_34, [2, 2, 2, 2], num_classes=num_classes, include_top=include_top)
+
+def resnet34(num_classes=1000,include_top=True):
+    # https://download.pytorch.org/models/resnet34-333f7ec4.pth
+    return ResNet(block_18_34,[3,4,6,3],num_classes=num_classes,include_top=include_top)
+
+def resnet50(num_classes=1000,include_top=True):
+    # https://download.pytorch.org/models/resnet50-19c8e357.pth
+    return ResNet(block_50_101_152,[3,4,6,3],num_classes=num_classes,include_top=include_top)
+
+def resnet101(num_classes=1000,include_top=True):
+    # https://download.pytorch.org/models/resnet101-5d3b4d8f.pth
+    return ResNet(block_50_101_152,[3,4,23,3],num_classes=num_classes,include_top=include_top)
+
+def resnet152(num_classes=1000,include_top=True):
+    # https://download.pytorch.org/models/resnet152-394f9c45.pth
+    return ResNet(block_50_101_152,[3,8,36,3],num_classes=num_classes,include_top=include_top)
+
+
+# a=resnet50()
+# print(a)
+# 可以输出一下网络结构，对比一下自己的是否正确，或者加载权重，试试。
---
+++ ResNet/train.py
@@ -0,0 +1,141 @@
+import os
+import sys
+import json
+import time
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms, datasets
+
+from model import resnet34
+
+
+def main():
+
+    batch_size = 16
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("| Device:",device)
+
+    data_transform = {
+        "train": transforms.Compose([transforms.RandomResizedCrop(224),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+        "val": transforms.Compose([transforms.Resize(256),
+                                   transforms.CenterCrop(224),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
+
+    # data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))  # get data root path
+    # image_path = os.path.join(data_root, "data_set", "flower_data")  # flower data set path
+    # assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
+
+    # 加载训练集和验证集
+    train_dir = os.path.join(os.getcwd(), 'data', 'train')
+    assert os.path.exists(train_dir), "{} path does not exist.".format(train_dir)
+    val_dir = os.path.join(os.getcwd(), 'data', 'val')
+    assert os.path.exists(train_dir), "{} path does not exist.".format(val_dir)
+
+    train_dataset = datasets.ImageFolder(root=train_dir,
+                                         transform=data_transform["train"])
+    train_num = len(train_dataset)
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size, shuffle=True,
+                                               num_workers=nw)
+
+    validate_dataset = datasets.ImageFolder(root=val_dir,
+                                            transform=data_transform["val"])
+    val_num = len(validate_dataset)
+    validate_loader = torch.utils.data.DataLoader(validate_dataset,
+                                                  batch_size=batch_size, shuffle=False,
+                                                  num_workers=nw)
+
+    # 加载类别信息
+    # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
+    flower_list = train_dataset.class_to_idx
+    cla_dict = dict((val, key) for key, val in flower_list.items())
+    # write dict into json file
+    json_str = json.dumps(cla_dict, indent=4)
+    with open('class_indices.json', 'w') as json_file:
+        json_file.write(json_str)
+
+    print("| Train dir:",train_dir)
+    print("| Train num:",train_num)
+    print("| Val dir:",val_dir)
+    print("| Val num:",val_num)
+    print("| Bath size:",batch_size)
+    print("| Number of workers:",nw)
+
+    net = resnet34()
+
+    # 加载官网的预训练权重，并修改最后一个全连接层的输出维度。
+    # download url: https://download.pytorch.org/models/resnet34-333f7ec4.pth
+    model_weight_path = "./weights/resnet34.pth"
+    assert os.path.exists(model_weight_path), "file {} does not exist.".format(model_weight_path)
+    net.load_state_dict(torch.load(model_weight_path, map_location='cpu'))
+    # for param in net.parameters():
+    #     param.requires_grad = False
+
+    # 修改最后一个全连接层的输出维度。
+    in_channel = net.fc.in_features     # 获取最后一个全连接层的输入维度。
+    net.fc = nn.Linear(in_channel, 5)   # 修改最后一个fc层的out_channel，改为5，因为我们要用5个类别的花分类数据集。
+    net.to(device)
+
+    # define loss function
+    loss_function = nn.CrossEntropyLoss()
+
+    # construct an optimizer
+    params = [p for p in net.parameters() if p.requires_grad]
+    optimizer = optim.Adam(params, lr=0.0001)
+
+    epochs = 3
+    best_acc = 0.0
+    save_path = './weights/resnet34_flower.pth'
+    train_steps = len(train_loader)
+    print("training.....")
+    start_time=time.time()
+    for epoch in range(epochs):
+        # train
+        net.train()
+        epoch_loss = 0.0
+        for step, data in enumerate(train_loader):
+            images, labels = data
+            optimizer.zero_grad()
+            logits = net(images.to(device))
+            loss = loss_function(logits, labels.to(device))
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            epoch_loss += loss.item()
+
+        # validate
+        net.eval()
+        acc = 0.0  # accumulate accurate number / epoch
+        with torch.no_grad():
+            for val_data in validate_loader:
+                val_images, val_labels = val_data
+                outputs = net(val_images.to(device))
+                # loss = loss_function(outputs, test_labels)
+                predict_y = torch.max(outputs, dim=1)[1]
+                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+
+        epoch_loss_eva=epoch_loss/train_steps
+        val_accurate = acc / val_num
+
+        if val_accurate > best_acc:
+            best_acc = val_accurate
+            torch.save(net.state_dict(), save_path)
+
+        print('Epoch:{:3d},  train loss:{:5.3f},  test accuracy:{:5.3f}'.format(
+            epoch + 1, epoch_loss_eva, val_accurate))
+
+    end_time=time.time()
+    print("| Time cost:",end_time-start_time)
+    print('Done.')
+
+
+if __name__ == '__main__':
+    main()
